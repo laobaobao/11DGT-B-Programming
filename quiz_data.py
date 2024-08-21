d9d51774165886cb8f3140fc0dfd9e26aa0b9b4d
laobaobao <168611651+laobaobao@users.noreply.github.com>
2024-08-21 13:58:46 +1200

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,102 @@
+quiz_data = [
+    { # question 1
+        "question": "What is the capital of Australia?",
+        "choices": ["Melbourne", "Brisbane", "Canberra", "Sydney"],
+        "answer": "Canberra" # 3rd option
+    },
+    { # question 2
+        "question": "What is the chemical element for Copper?",
+        "choices": ["Cu", "C", "Ca", "Co"],
+        "answer": "Cu" # 1st option
+    },
+    { # question 3
+        "question": "What year did the Titanic sink?",
+        "choices": ["1945", "1912", "1913", "1922"],
+        "answer": "1912" # 2nd option
+    },
+    { # question 4
+        "question": "What is the powerhouse of the cell?",
+        "choices": ["Nucleus", "Choloplast", "Mitochondria", "Membrane"],
+        "answer": "Mitochondria" # 3rd option
+    },
+    { # question 5
+        "question": "What year was Mickey Mouse created?",
+        "choices": ["1934", "1955", "1928", "1926"],
+        "answer": "1928" # 3rd option
+    },
+    { # question 6
+        "question": "Who was the 33rd President of the USA?",
+        "choices": ["Gerald Ford", "Donald Trump", "John F. Kennedy", "Harry S. Truman"],
+        "answer": "Harry S. Truman" # 4th option
+    },
+    { # question 7
+        "question": "Which ENHYPEN member ranked 1st place on the finale of I-LAND?" ,
+        "choices": ["Jungwon", "Heeseung", "Sunoo", "Ni-ki"],
+        "answer": "Jungwon" # 1st option
+    },
+    { # question 8
+        "question": "Which country will hold the 2028 Summer Olympics?",
+        "choices": ["Australia", "France", "UK", "USA"],
+        "answer": "USA" # 4th option
+    },
+    { # question 9
+        "question": "How many chambers are in a human heart?",
+        "choices": ["5", "4", "2", "1"],
+        "answer": "4" # 2nd option
+    },
+    { # question 10
+        "question": "Which state does Burger King originate from?",
+        "choices": ["Hawaii", "California", "Florida", "New York"],
+        "answer": "Florida" # 3rd option
+    },
+    { # question 11
+        "question": "How many timezones are used in Russia?",
+        "choices": ["11", "12", "8", "9"],
+        "answer": "11" # 1st option
+    },
+    { # question 12
+        "question": "Which nation's passport is the most powerful as of July 2024?",
+        "choices": ["USA", "Singapore", "Switzerland", "Finland"],
+        "answer": "Singapore" # 2nd option
+    },
+    { # question 13
+        "question": "Who was the first European to discover New Zealand?",
+        "choices": ["James Cook", "Abel Tasman", "William Hobson", "none of the above"],
+        "answer": "Abel Tasman" # 2nd option
+    },
+    { # question 14
+        "question": "In which year was the current flag of Jamaica adopted?",
+        "choices": ["2004", "1981", "1949", "1962"],
+        "answer": "1962" # 4th option
+    },
+    { # question 15
+        "question": "Which planet in our solar system has the most moons?",
+        "choices": ["Jupiter", "Uranus", "Saturn", "Earth"],
+        "answer": "Saturn" # 3rd option
+    },
+    { # question 16
+        "question": "What is King Charles III's surname?",
+        "choices": ["Arthur", "Wales", "Windsor", "Mountbatten-Windsor"],
+        "answer": "Mountbatten-Windsor" # 4th option
+    },
+    { # question 17
+        "question": "What was New York City's original name?",
+        "choices": ["New Amsterdam", "New Liverpool", "New Brussels", "Greater Manhattan"],
+        "answer": "New Amsterdam" # 1st option
+    },
+    { # question 18
+        "question": "In which country was the Caesar salad invented?",
+        "choices": ["Poland", "Italy", "USA", "Mexico"],
+        "answer": "Mexico" # 4th option
+    },
+    { # question 19
+        "question": "Which of the following is not an NCEA subject?",
+        "choices": ["Computer Science", "Classical Studies", "Driver's License (Class 1)","Latin"],
+        "answer": "Computer Science" # 1st option
+    },
+    { # question 20
+        "question": "What is a group of crows called?",
+        "choices": ["Unkindness", "Murder", "Pounce", "Parliament"],
+        "answer": "Murder" # 2nd option
+    },
+]
